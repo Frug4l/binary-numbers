a58b8b357a017c57bc0773340d96d5d43105f2d8
Frug4l <maximencoda@gmail.com>
2025-11-25 23:32:33 +0300

    исправил проверку кратности

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ class BinaryMultipleOfThree:
         return r'\b[01]+\b'
     
     def is_multiple_of_three(self, binary_str):
-        return len(binary_str) % 3 == 0
+        decimal_value = int(binary_str, 2)
+        return decimal_value % 3 == 0
     
     def find_binary_numbers(self, text):
         pattern = self.create_regex()
