b942c473bd6732d2016792553b9dcc82c7d86446
Frug4l <maximencoda@gmail.com>
2025-11-26 23:13:42 +0300

    пользовательский ввод

--- main.py
+++ main.py
@@ -15,3 +15,14 @@ class BinaryMultipleOfThree:
     def find_multiples_of_three(self, text):
         binary_numbers = self.find_binary_numbers(text)
         return [num for num in binary_numbers if self.is_multiple_of_three(num)]
+    
+    def analyze_text(self, text):
+        all_binary = self.find_binary_numbers(text)
+        multiples = self.find_multiples_of_three(text)
+        print("Найдено чисел: " + str(len(all_binary)))
+        return multiples
+
+validator = BinaryMultipleOfThree()
+text = input("Введите текст: ")
+result = validator.analyze_text(text)
+print("Результат: " + str(result))
